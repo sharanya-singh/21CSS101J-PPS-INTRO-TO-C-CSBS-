eb95a7ffac85cae0e6b59fa7c4d13e96294017c2
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-21 19:47:01 +0530

    Create 2.c

---
+++ IF/2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  printf("RA2211042010006")
+    {
+	int num1=1;
+	int num2=2;
+	if(num1<num2)		//test-condition
+	{
+		printf(“num1 is smaller than num2”);
+	}
+	return 0;
+}

4196e67a79d19ea2b6fba8e87b3d9820e3e32b2f
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-21 19:15:02 +0530

    Create day of week2.c

---
+++ day of week2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  printf("RA2211042010006")
+    int day = 4;
+
+switch (day) {
+  case 6:
+    printf(“Today is Saturday”);
+    break;
+  case 7:
+    printf(“Today is Sunday”);
+    break;
+  default:
+    printf(“Looking forward to the Weekend”);
+}

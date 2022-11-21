527b62aeeed91e587004532c6988348b4c7ac5b3
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-21 19:58:47 +0530

    Create else general.c

---
+++ IF/else general.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+  if (/condition/) {
+   /// block of code to be executed if the condition is true/
+ } 
+else { 
+   /// block of code to be executed if the condition is false/
+} 

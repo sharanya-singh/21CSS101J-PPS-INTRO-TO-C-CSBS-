dfa6263ff9b7536c3b2ed20d97702444355b1cc7
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-21 19:08:07 +0530

    Create positive or negative integer.c

---
+++ positive or negative integer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  printf("RA2211042010006")
+  int myNum = 10; // Is this a positive or negative number?`
+
+if (myNum > 0)
+printf(“The value is a positive number.”);
+else if (myNum < 0)
+  printf(“The value is a negative number.”);
+else
+  printf(“The value is 0.”);
+}

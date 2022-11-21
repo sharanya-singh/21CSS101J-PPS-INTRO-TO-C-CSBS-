23e4bcbc9b77fb3722dff4034a4b7c0650d96d06
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-21 19:52:07 +0530

    Create nested general.c

---
+++ IF/nested general.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+   if (test - expression 1) {
+    statement1;
+} else if (test - expression 2) {
+    Statement2;
+} else if (test - expression 3) {
+    Statement3;
+} else if (test - expression n) {
+    Statement n;
+} else {
+    default;
+}
+Statement x; 
+}

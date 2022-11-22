d7bfac3d52502058bf807d3f54bf5b8e28d5b54b
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-22 10:32:20 +0530

    Create LANGUAGE.C

---
+++ switch/LANGUAGE.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+  printf("RA2211042010006")
+int language = 10;
+  switch (language) {
+  case 1:
+    printf(“C#\n”);
+    break;
+  case 2:
+    printf(“C\n”);
+    break;
+  case 3:
+    printf(“C++\n”);
+    break;
+  default:
+    printf(“Other programming language\n”);}}

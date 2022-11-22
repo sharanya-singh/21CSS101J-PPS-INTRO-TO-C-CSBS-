2539161fac65612a697d47eccccacc52c3ce9d39
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-22 10:48:37 +0530

    Create numbers3.c

---
+++ switch/numbers3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  printf(“RA2211042010006”)
+     int num=2;
+     switch(num+2)
+     {
+         case 1:
+           printf("Case1: Value is: %d", num);
+         case 2:
+           printf("Case1: Value is: %d", num);
+         case 3:
+           printf("Case1: Value is: %d", num);
+         default:
+           printf("Default: Value is: %d", num);
+    }
+    return 0;
+}

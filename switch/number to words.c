f2c6b2f88d7fdf37e1198f8edced5e7d3a9613ee
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-22 11:03:24 +0530

    Create number to words.c

---
+++ switch/number to words.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+  printf("RA2211042010006")
+  int a = 9;
+switch (a) {
+ case 1: printf("I am One\n");
+         break;
+ case 2: printf("I am Two\n");
+         break;
+ case 3: printf("I an Three\n");
+         break;
+ case 4: printf("I am Four\n");
+         break;
+ case 5: printf("I am Five\n");
+         break;
+ case 6: printf("I am Six\n");
+         break;
+ case 7: printf("I am Seven\n");
+         break;
+ case 8: printf("I am Eight\n");
+ case 9: printf("I am Nine\n");
+ case 0: printf("I am Zero\n");
+ default: printf("I am default\n");
+}

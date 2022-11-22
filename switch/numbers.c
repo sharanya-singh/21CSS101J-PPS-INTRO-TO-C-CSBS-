0a84d67a1c20fb68319f75f13c304abbf369f9a7
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-22 10:34:19 +0530

    Create numbers.c

---
+++ switch/numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+  Printf(“RA2211042010006”)
+int number=5;
+switch (number) {
+  case 1:
+  case 2:
+  case 3:
+    printf(“One, Two, or Three.\n”);
+    break;
+  case 4:
+  case 5:
+  case 6:
+    printf(“Four, Five, or Six.\n”);
+    break;
+  default:
+    printf(“Greater than Six.\n”);}}

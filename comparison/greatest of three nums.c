b080c174b582cef4f0a030bb78f96a0dd82a6521
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-22 10:39:35 +0530

    Create greatest of three nums.c

---
+++ comparison/greatest of three nums.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void main( )
+{
+  Printf(“RA2211042010006”)
+int a,b,c;
+clrscr();
+printf(“Please Enter three number”);
+scanf(“%d%d%d”,&a,&b,&c);
+if(a>b)
+{
+if(a>c)
+{
+printf(“a is greatest number”);
+}
+else
+{
+printf(“c is greatest number”);
+}
+}
+else
+{
+if(b>c)
+{
+printf(“b is greatest number”);
+}
+else
+{
+printf(“c is greatest”);
+}
+}
+getch();
+}

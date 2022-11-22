b792afba7afca4bc14d3341537ec9244a37c91ca
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-22 10:56:41 +0530

    Create alphabet.c

---
+++ switch/alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main() {
+ printf("RA2211042010006")
+   char n='C';
+  switch(n)
+  {
+    case 'A':
+    case 'B':
+       printf("A and B\n");
+      break;
+       
+    case 'C':
+    case 'D':
+      printf("C and D\n");
+      break;
+       
+    default:printf("Alphabet is greater than D\n");
+      break;
+  }
+  return 0;
+         
+}

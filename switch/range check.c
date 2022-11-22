1d86dcc9f700d6f4b8aface02141b7ee577b0dc0
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-22 10:31:27 +0530

    Create range check.c

---
+++ switch/range check.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+  printf("RA2211042010006")
+        int num = 8;
+        switch (num) {
+            case 7:
+                printf(“Value is 7”);
+                break;
+            case 8:
+                printf(“Value is 8”);
+                break;
+            case 9:
+                printf(“Value is 9”);
+                break;
+            default:
+                printf(“Out of range”);
+                break;
+        }
+        return 0;
+    }

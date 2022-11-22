5fa99b2c06237b7957cdc14cfd5dfb339b997271
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-22 10:53:57 +0530

    Create choices.c

---
+++ switch/choices.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+// Driver Code
+int main()
+{
+  printf("RA2211042010006")
+    char x = 'A';
+    switch (x) {
+    case 'A':
+        printf("Choice is A");
+        break;
+    case 'B':
+        printf("Choice is B");
+        break;
+    case 'C':
+        printf("Choice is C");
+        break;
+    default:
+        printf("Choice other than A, B and C");
+        break;
+    }
+    return 0;
+}

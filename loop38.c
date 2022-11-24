3d1b728ae5855fedb3ca5fec58cb4d44ae3c0419
divyanshikochar <113873694+divyanshikochar@users.noreply.github.com>
2022-11-24 23:46:32 +0530

    sum of n numbers

---
+++ loop38.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+   int i = 1,n, sum = 0;
+   printf("\nEnter the value for n: ");
+   scanf("%d",&n);
+   while (i<=n)
+   {sum = sum + i;
+   i++;
+   }
+   printf("The sum of n numbers is: %d", sum);
+   return 0;
+}

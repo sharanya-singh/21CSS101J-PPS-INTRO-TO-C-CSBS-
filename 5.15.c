85844ec8655bd93e1b15ee924f0cafe1067ac3c4
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-26 00:15:54 +0530

    Create 5.15.c

---
+++ 5.15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+printf("RA2211042010006")
+  main( ) 
+{
+int radius ;
+float area, perimeter ;
+printf ( "\nEnter radius of a circle " ) ;
+  scanf ( "%d", &radius ) ;
+areaperi ( radius, &area, &perimeter ) ;
+printf ( "Area = %f", area ) ;
+printf ( "\nPerimeter = %f", perimeter ) ; 
+}
+areaperi ( int r, float *a, float *p ) 
+{
+*a = 3.14 * r * r ;
+*p = 2 * 3.14 * r ; 
+}

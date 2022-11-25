4b89891c86c8f431cb62c11d7943b72422516c94
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-26 00:30:47 +0530

    Create 5.17.c

---
+++ 5.17.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+printf("RA2211042010006")
+  main( )
+{
+int i = 45, c ;
+c = multiply ( i * 1000 ) ;
+  printf ( "\n%d", c ) ;
+}
+check(int ch)
+{
+if ( ch >= 40000 ) return ( ch / 10 ) ;
+else
+return ( 10 ) ;
+}

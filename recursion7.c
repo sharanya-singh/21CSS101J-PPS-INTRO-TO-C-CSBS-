a7bccd061886060e8923615cd6e8bd5a114c4a1a
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-26 00:26:18 +0530

    Create recursion7.c

---
+++ recursion7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+printf("RA2211042010006")
+  main( ) 
+{
+int a = 5, b = 2, c ;
+c = add ( a, b ) ;
+printf ( "sum = %d", c ) ;
+}
+add ( int i, int j ) 
+{
+int sum ;
+  sum = i + j ; 
+  return sum ;
+}

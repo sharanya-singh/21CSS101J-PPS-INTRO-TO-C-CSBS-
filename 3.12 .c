deff466f0ec7ace2692c240164cab3f4d41fe17a
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-25 19:25:36 +0530

    Create 3.12 .c

---
+++ 3.12 .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+  printf("RA2211042010006")
+  main( ) {
+int i=1; switch ( i - 2 ) {
+case -1 :
+printf ( "\nFeeding fish" ) ;
+case 0 :
+printf ( "\nWeeding grass" ) ;
+case 1 :
+printf ( "\nmending roof" ) ;
+default :
+printf ( "\nJust to survive" ) ;
+}
+}

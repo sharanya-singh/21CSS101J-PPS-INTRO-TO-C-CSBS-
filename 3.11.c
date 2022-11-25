79fd020bab692fc48c15069f4115b0fb7a88574c
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-25 19:24:32 +0530

    Create 3.11.c

---
+++ 3.11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+  printf("RA2211042010006")
+main( ) 
+{
+int ch='a'+'b'; 
+  switch ( ch )
+{
+case 'a' :
+    case 'b' :
+printf ( "\nYou entered b" ) ; 
+    case 'A' :
+printf ( "\na as in ashar" ) ; 
+    case 'b' + 'a' :
+printf ( "\nYou entered a and b" ) ;
+  }
+}

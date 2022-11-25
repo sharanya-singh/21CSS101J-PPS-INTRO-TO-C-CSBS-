ce8f9d815338eaefe07f8a4c6ce73e38248ea439
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-26 00:05:34 +0530

    Create functions11.c

---
+++ functions11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+printf("RA2211042010006")
+  main( ) 
+{
+void gospel( ) ;
+gospel( ) ; 
+}
+void gospel( ) 
+{
+printf ( "\nViruses are electronic bandits..." ) ; 
+  printf ( "\nwho eat nuggets of information..." ) ; 
+  printf ( "\nand chunks of bytes..." ) ;
+printf ( "\nwhen you least expect..." ) ;
+}

0d6f3214757a7d93736775d90e4d570c79d82e90
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-26 00:33:20 +0530

    Create 5.19.c

---
+++ 5.19.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+printf("RA2211042010006")
+  main( )
+{
+void slogan( ) ;
+int c=5;
+c = slogan( ) ; 
+  printf ( "\n%d", c ) ;
+}
+void slogan( )
+{
+printf ( "\nOnly He men use C!" ) ; 
+}

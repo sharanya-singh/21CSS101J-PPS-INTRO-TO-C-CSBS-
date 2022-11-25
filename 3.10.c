02a796aba8e361e3549cfe90434c145bae102f82
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-25 19:23:30 +0530

    Create 3.10.c

---
+++ 3.10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+  printf("RA2211042010006")
+main( ) 
+{
+int k,j=2; 
+  switch ( k = j + 1 ) 
+{
+case 0 :
+printf ( "\nTailor") ;
+case 1 :
+printf ( "\nTutor") ;
+case 2 :
+printf ( "\nTramp") ;
+default :
+printf ( "\nPure Simple Egghead!" ) ;
+} 
+}

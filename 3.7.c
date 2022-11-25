699ab93da2fda0bdc5213c6d9a66e9a788540269
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-25 19:18:36 +0530

    Create 3.7.c

---
+++ 3.7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+  printf("RA2211042010006")
+  main( ) 
+{
+int goals ;
+printf ( "Enter the number of goals scored against India" ) ; 
+  scanf ( "%d", &goals ) ;
+if ( goals <= 5 ) 
+  goto sos ;
+else
+{
+sos :
+printf ( "To err is human!" ) ;
+ printf ( "About time soccer players learnt C\n" ) ; 
+  printf ( "and said goodbye! adieu! to soccer" ) ; 
+  exit( ) ; 
+  /* terminates program execution */

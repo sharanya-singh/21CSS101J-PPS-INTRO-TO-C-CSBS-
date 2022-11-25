99e40c8e33d82805f9bad4cde3d9037a2243ec2e
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-25 19:11:11 +0530

    Create 3.5.c

---
+++ 3.5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+  printf("RA2211042010006")
+  main( ) 
+{
+char ch ;
+printf ( "Enter any of the alphabet a, b, or c " ) ; 
+  scanf ( "%c", &ch ) ;
+  switch ( ch ) 
+  {
+case 'a' : 
+case 'A' :
+printf ( "a as in ashar" ) ;
+break ; case 'b' :
+case 'B' :
+printf ( "b as in brain" ) ;
+      break ;
+case 'c' : 
+case 'C' :
+printf ( "c as in cookie" ) ;
+break ; 
+    default :
+printf ( "wish you knew what are alphabets" ) ; 
+  }
+}

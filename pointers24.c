7d7f7ebcc27ebaddc30478caa635037f55e9e48f
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-26 00:08:41 +0530

    Create pointers24.c

---
+++ pointers24.c
@@ -0,0 +1,9 @@
+#Include <stdio.h>
+printf("RA2211042010006")
+  main( ) 
+{
+int i=3;
+printf ( "\nAddress of i = %u", &i ) ;
+  printf ( "\nValue of i = %d", i ) ; 
+  printf ( "\nValue of i = %d", *( &i ) ) ;
+}

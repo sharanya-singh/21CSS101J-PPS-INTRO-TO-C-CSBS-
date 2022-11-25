03f5431e4c5a60918e216e1c0655d3e72074ab33
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-25 19:08:59 +0530

    Create 3.4.c

---
+++ 3.4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+  printf("RA2211042010006")
+  main( )
+{
+int i = 2 ;
+switch ( i )
+{
+case 1 :
+printf ( "I am in case 1 \n" ) ;
+case 2 :
+printf ( "I am in case 2 \n" ) ;
+case 3 :
+printf ( "I am in case 3 \n" ) ;
+default :
+printf ( "I am in default \n" ) ;
+} 
+}

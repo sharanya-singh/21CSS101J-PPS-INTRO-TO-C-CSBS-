22a19452bbbedf39d84779d8f19454c970f82b67
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-25 19:22:30 +0530

    Create 3.9.c

---
+++ 3.9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+  printf("RA2211042010006")
+main( ) 
+{
+int i=0; switch ( i ) 
+{
+case 0 :
+printf ( "\nCustomers are dicey" ) ;
+case 1 :
+printf ( "\nMarkets are pricey" ) ;
+case 2 :
+printf ( "\nInvestors are moody" ) ;
+case 3 :
+printf ( "\nAt least employees are good" ) ;
+}
+}

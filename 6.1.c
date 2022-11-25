bb1be439c7f95dc8cb6c4e4bf1a1fff25a7be026
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-26 00:44:51 +0530

    Create 6.1.c

---
+++ 6.1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+printf("RA2211042010006")
+  main( ) 
+{
+unsigned char ch ;
+for ( ch = 0 ; ch <= 254 ; ch++ ) 
+  printf ( "\n%d %c", ch, ch ) ;
+printf ( "\n%d %c", ch, ch ) ;
+}

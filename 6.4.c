d4ee8290b05dc2203dffb4d8aca7c7a8cafaa5ac
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-26 00:48:10 +0530

    Create 6.4.c

---
+++ 6.4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+printf("RA2211042010006")
+  int i ; main( ) 
+  {
+printf ( "\ni = %d", i ) ;
+increment( ) ;
+    increment( ) ;
+    decrement( ) ; 
+    decrement( ) ;
+}
+increment( ) 
+{
+i=i+1;
+printf ( "\non incrementing i = %d", i ) ; 
+}
+decrement( )
+ {
+i=i-1;
+printf ( "\non decrementing i = %d", i ) ; 
+}

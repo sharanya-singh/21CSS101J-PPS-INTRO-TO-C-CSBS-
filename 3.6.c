cbd578452d2b576f735134ab566aefa88ca96e1b
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-25 19:16:07 +0530

    Create 3.6.c

---
+++ 3.6.c
@@ -0,0 +1,19 @@
+#incude <stdio.h>
+int main()
+  printf("RA2211042010006")
+  main( ) 
+{
+int i,j;
+printf ( "Enter value of i" ) ; 
+  scanf ( "%d”, &i ) ;
+switch ( i ) 
+         {
+printf ( "Hello" ) ;
+           case 1 :
+j = 10 ;
+break ; 
+           case 2 :
+j = 20 ;
+break ; 
+         }
+}

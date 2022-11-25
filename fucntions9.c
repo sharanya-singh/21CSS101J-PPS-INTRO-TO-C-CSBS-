d176d480a1fd8af6a7329b6d8c838e696a078617
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-26 00:02:10 +0530

    Create fucntions9.c

---
+++ fucntions9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+printf("RA211042010006")
+  main( )
+{
+float a, b ;
+printf ( "\nEnter any number " ) ;
+  scanf ( "%f", &a ) ;
+b = square ( a ) ;
+printf ( "\nSquare of %f is %f", a, b ) ; 
+}
+square ( float x )
+{
+float y ;
+y=x*x;
+return ( y ) ;
+}

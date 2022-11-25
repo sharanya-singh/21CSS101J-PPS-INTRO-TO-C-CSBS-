fdfde7704496c67d8a892df4e3236fc0d00e5b95
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-26 00:32:15 +0530

    Create 5.18.c

---
+++ 5.18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+printf("RA2211042010006")
+  main( ) {
+float area ;
+int radius=1;
+area = circle ( radius ) ;
+  printf ( "\n%f", area ) ;
+}
+circle(int r)
+  {
+float a ;
+a = 3.14 * r * r ; 
+  return ( a ) ;
+}

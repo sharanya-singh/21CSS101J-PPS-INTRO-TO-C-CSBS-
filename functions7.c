71f908808d181e95972630bedd24672f03717b9c
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-25 23:58:44 +0530

    Create functions7.c

---
+++ functions7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+  printf("RA2211042010006")
+  fun( )
+{
+char ch ;
+printf ( "\nEnter any alphabet " ) ;
+  scanf ( "%c", &ch ) ;
+if ( ch >= 65 && ch <= 90 ) 
+  return ( ch ) ;
+else
+return ( ch + 32 ) ;
+}

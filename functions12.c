60d5d547bc062ec5d2ffa10492e69b5ed3980c89
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-11-26 00:12:32 +0530

    Create functions12.c

---
+++ functions12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+printf("RA2211042010006")
+  main( ) 
+{
+int a=10,b=20;
+swapv ( a, b ) ;
+printf ( "\na = %d b = %d", a, b ) ; 
+}
+swapv(int x,int y)
+{
+int t;
+t=x;
+x=y; 
+y=t;
+printf ( "\nx = %d y = %d", x, y ) ; 
+}
